c77ce69acc1af629332d3b042e94e6862b99ef9f
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-07-01 17:33:30 +0200

    Create install_catch.sh

---
+++ build/install_catch.sh
@@ -0,0 +1,10 @@
+﻿﻿#!/bin/sh
+
+git clone https://github.com/catchorg/Catch2.git
+cd Catch2
+git checkout tags/v3.3.0
+cmake -Bbuild -S. -DBUILD_TESTING=OFF
+cmake --build build/ --target install --config Debug
+cmake --build build/ --target install --config Release
+cmake --build build/ --target install --config RelWithDebInfo
+cd ..

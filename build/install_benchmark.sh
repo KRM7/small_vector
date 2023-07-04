bbc80ffe522489b42e0acc42d254abd92250bacc
KRM7 <70973547+KRM7@users.noreply.github.com>
2023-07-04 22:22:40 +0200

    update workflows

---
+++ build/install_benchmark.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/sh
+
+git clone https://github.com/google/benchmark.git
+cd benchmark
+git checkout tags/v1.8.1
+cmake -Bbuild -S. -DBENCHMARK_ENABLE_GTEST_TESTS=OFF
+cmake --build build/ --target install --config Debug
+cmake --build build/ --target install --config Release
+cmake --build build/ --target install --config RelWithDebInfo
+cd ..

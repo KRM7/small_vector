3db2460ba6a5b0f9ba20b6516982398496a707f3
KRM7 <70973547+KRM7@users.noreply.github.com>
2024-01-06 15:44:44 +0100

    fix github workflows

--- build/install_benchmark.sh
+++ build/install_benchmark.sh
@@ -1,10 +1,27 @@
-﻿#!/bin/sh
+﻿#!/bin/bash
 
-git clone https://github.com/google/benchmark.git
-cd benchmark
-git checkout tags/v1.8.1
-cmake -Bbuild -S. -DBENCHMARK_ENABLE_GTEST_TESTS=OFF
-cmake --build build/ --target install --config Debug
-cmake --build build/ --target install --config Release
-cmake --build build/ --target install --config RelWithDebInfo
-cd ..
+echo -e "Installing google-benchmark...\n"
+
+cmake --version
+
+BUILD_DIR=$(dirname $(realpath "$0"))/../build
+echo -e "\nThe build directory is ${BUILD_DIR}.\n"
+
+git clone -b v1.8.1 https://github.com/google/benchmark.git $BUILD_DIR/benchmark
+BENCHMARK_BUILD_DIR=$BUILD_DIR/benchmark/build
+echo -e "\nThe Catch build directory is ${BENCHMARK_BUILD_DIR}.\n"
+
+echo -e "Installing Debug configuration.\n"
+cmake -S $BENCHMARK_BUILD_DIR/.. -B $BENCHMARK_BUILD_DIR -DCMAKE_BUILD_TYPE=Debug -DBENCHMARK_ENABLE_GTEST_TESTS=OFF "$@"
+cmake --build $BENCHMARK_BUILD_DIR --config Debug --parallel
+cmake --install $BENCHMARK_BUILD_DIR --config Debug
+
+echo -e "Installing RelWithDebInfo configuration.\n"
+cmake -S $BENCHMARK_BUILD_DIR/.. -B $BENCHMARK_BUILD_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBENCHMARK_ENABLE_GTEST_TESTS=OFF "$@"
+cmake --build $BENCHMARK_BUILD_DIR --config RelWithDebInfo --parallel
+cmake --install $BENCHMARK_BUILD_DIR --config RelWithDebInfo
+
+echo -e "Installing Release configuration.\n"
+cmake -S $BENCHMARK_BUILD_DIR/.. -B $BENCHMARK_BUILD_DIR -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_GTEST_TESTS=OFF "$@"
+cmake --build $BENCHMARK_BUILD_DIR --config Release --parallel
+cmake --install $BENCHMARK_BUILD_DIR --config Release
